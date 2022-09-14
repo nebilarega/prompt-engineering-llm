6d6c94fcf57e809733da37d00bebae1ef09ae8ab
Nebil Arega <36981187+nebilarega@users.noreply.github.com>
2022-09-14 22:13:25 +0300

    Added basic cohere generative call

--- .history/scripts/basic_api_call_20220914214127.py
+++
@@ -1,9 +0,0 @@
-import cohere
-try:
-  response = co.generate(
-      model='invalid-model',
-      prompt='sample prompt')
-except cohere.CohereError as e:
-    print(e.message)
-    print(e.http_status)
-    print(e.headers)
